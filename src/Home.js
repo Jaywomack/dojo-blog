1fb3c40f6b08af308c1b2f9c92ff74dbebbcea3b
jaywomack <womackjw@hotmail.com>
2021-01-27 19:23:32 -0800

    Using useEffect hook to access mock data from json-server also catching errors

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'my new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('Could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      <BlogList blogs={blogs} title='All Blogs' />
     </div>
   );
 };
